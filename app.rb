31c4a8c1b512f8be4dafe555aff0be17fe466d8b
lewis nyoike <nyoikelewis@gmail.com>
2017-07-11 07:34:54 +0300

    followed content

--- app.rb
+++ app.rb
@@ -1,16 +1,19 @@
-require("sinatra")
-require("sinatra/reloader")
-also_reload("lib/**/*.rb")
+require('sinatra')
+require('sinatra/reloader')
+also_reload('lib/**/*.rb')
 require('./lib/todo')
+require('pg')
 
-get("/") do
-  @tasks = Task.all()
+DB = PG.connect(dbname: 'to_do')
+
+get('/') do
+  @tasks = Task.all
   erb(:index)
 end
 
-post("/task") do
-  description = params.fetch("description")
+post('/task') do
+  description = params.fetch('description')
   task = Task.new(description)
-  task.save()
+  task.save
   erb(:success)
 end
